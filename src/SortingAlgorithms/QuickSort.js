7fdc1fcdb1e03fd56e3db4191e843d9f0435d46e
Manthan Bhardwaj <95605134+Manthan0786@users.noreply.github.com>
2022-02-24 00:19:09 -0700

    Update QuickSort.js

--- src/SortingAlgorithms/QuickSort.js
+++ src/SortingAlgorithms/QuickSort.js
@@ -1,30 +1,30 @@
-export function quicksort(array) {
+export function quicksort(array) {                               //calling original array
   const animation = [];
   if (array.length <= 1) {
     return;
   }
   quicksortHelper(array, 0, array.length - 1, animation);
   print(array);
-  return animation;
+  return animation;                                           //returning animations for visualization
 }
 
 function quicksortHelper(arr, s, e, animation) {
   if (s >= e) {
     return;
   }
-  let p = partition(arr, s, e, animation);
+  let p = partition(arr, s, e, animation);                        //function to divide array in two parts
   quicksortHelper(arr, s, p - 1, animation);
   quicksortHelper(arr, p + 1, e, animation);
 }
 
 function partition(array, s, e, animation) {
-  var pivot = array[e];
-  var pindex = s;
+  var pivot = array[e];                                           //select last element of array as pivot
+  var pindex = s;                                        
   for (let i = s; i < e; i++) {
-    animation.push(["comparison1", i, e]);
-    animation.push(["comparison2", i, e]);
+    animation.push(["comparison1", i, e]);                       //Comparing value at i and e so push them to change their color
+    animation.push(["comparison2", i, e]);                       //to turn them into secondary color
     if (array[i] <= pivot) {
-      animation.push(["comparison1", i, pindex]);
+      animation.push(["comparison1", i, pindex]);                
       animation.push(["swap", i, array[pindex]]);
       animation.push(["swap", pindex, array[i]]);
       animation.push(["comparison2", i, pindex]);
